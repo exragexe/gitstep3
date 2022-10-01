9bf5bf31ad8abf480360b1c5cc214f29afcbb9b4
yaros <yaroslavdisiak@gmail.com>
2022-10-01 15:37:44 +0300

    lab 1 done

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #dev
+num1 = int(input("Enter diapazon first number: "))
+num2 = int(input("Enter diapazon second number: "))
+num3 = 1
+
+for i in  range(num1,num2 +1):
+    print(i)
+
+else:
+    print("Done!")
