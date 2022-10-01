273c017a94bcdc9b964eaaed7dfd78aacf5efcb4
yaros <yaroslavdisiak@gmail.com>
2022-10-01 18:56:26 +0300

    hm 3 done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #dev
+num1 = int(input("Enter diapazon first number: "))
+num2 = int(input("Enter diapazon second number: "))
+for i in range(num1,num2+1):
+    if i %3==0 ^ i%5==0:
+        print(f"Fizz Buzz ")
+    elif i %5==0:
+        print(f"Buzz ")
+    elif i%3==0:
+        print(f"Fizz " )
+    else:
+        print(i)
