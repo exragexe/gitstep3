b3e53cededcc8702133a73ab209b1ad71e8ae3d8
yaros <yaroslavdisiak@gmail.com>
2022-10-01 15:41:28 +0300

    lab 4 done

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #dev
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+z=[]
+
+while x>=y :
+    z.append(x)
+    x-=1
+    for i in z[::-1]:
+      print(i)
+      break
+while x<=y :
+    z.append(y)
+    y-=1
+    for i in z[::-1]:
+       print(i)
+       break
