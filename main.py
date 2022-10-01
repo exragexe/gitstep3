ac8ef7d379a85fc23287acef182cead693d8afd7
yaros <yaroslavdisiak@gmail.com>
2022-10-01 18:36:05 +0300

    hm 2 done

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 #dev
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+z=[]
+
+
+while x>=y:
+    z.append(x)
+    x-=1
+    for i in z[::-28]:
+        if i %7==0:
+            print(i )
+            break
+
+while x<=y:
+    z.append(y)
+    y-=1
+    for i in z[::-28]:
+        if i %7==0:
+
+            print(i )
+            break
