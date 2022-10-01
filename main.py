e36064d0c923029bd598e78cdbd72379261c4d0e
yaros <yaroslavdisiak@gmail.com>
2022-10-01 15:44:41 +0300

    hm 1 done

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #dev
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+for i in range(num1, num2 +1):
+    if i % 7==0:
+        print(i)
+else:
+            for i in range(num2,num1+1):
+                if i % 7 == 0:
+                    print(i)
