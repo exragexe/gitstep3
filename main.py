6be99eed390af0948eff814fd9d91c73ee961fe4
yaros <yaroslavdisiak@gmail.com>
2022-10-01 15:43:52 +0300

    lab 5 done

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #dev
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+for i in range(num1,num2+1):
+     if i %2==1:
+        print(i)
+
+else:
+            for i in range(num2,num1+1):
+                if i % 2 == 1:
+                    print(i)
